4b12ec8134de220c0565ec6ae6dca438c18758e4
GiovanniM3n0n <giovannimenon52@gmail.com>
2024-08-26 19:42:49 -0300

    Revert "Feat() Application start"
    This reverts commit 4a9ec1883cbe334b1cde9fcc1309a7e482ad0ae5.

--- frontend/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['localhost'], // Adicione domínios se você estiver carregando imagens de fontes externas
-    },
-};
-
-export default nextConfig;
